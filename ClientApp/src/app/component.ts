1393f476d46248114de8b2601dc0f3830d014a28
hanichu <hendrymino@gmail.com>
2019-07-08 17:28:49 +0200

    Done some random test

--- ClientApp/src/app/component.ts
+++ ClientApp/src/app/component.ts
@@ -9,20 +9,69 @@ import { Product } from "./product.model";
 export class ProductComponent {
   model: Model = new Model();
 
-  constructor(ref: ApplicationRef) {
-    (<any>window).appRef = ref;
-    (<any>window).model = this.model;
+  //constructor(ref: ApplicationRef) {
+  //  (<any>window).appRef = ref;
+  //  (<any>window).model = this.model;
+  //}
+
+  //getProductByPosition(position: number): Product {
+  //  return this.model.getProducts()[position];
+  //}
+
+  getProduct(key: number): Product {
+    return this.model.getProduct(key);
   }
 
-  getProductByPosition(position: number): Product {
-    return this.model.getProducts()[position];
+  getProducts(): Product[] {
+    return this.model.getProducts();
   }
 
-  getClassesByPosition(position: number): string {
-    let product = this.getProductByPosition(position);
-    return "p-2 " + (product.price < 50 ? "bg-info" : "bg-warning");
-  }
+  //  getProductCount(): number {
+  //    console.log("getProductCount invoked");
+  //    return this.getProducts().length;
+  //  }
+
+  //  targetName: string = "Kayak";
+
+  //  counter: number = 1;
+
+  //  get nextProduct(): Product {
+  //    return this.model.getProducts().shift();
+  //  }
+
+  //  getProductPrice(index: number): number {
+  //    return Math.floor(this.getProduct(index).price);
+  //  }
 }
+
+
+
+
+//import { ApplicationRef, Component } from "@angular/core";
+//import { Model } from "./repository.model";
+//import { Product } from "./product.model";
+
+//@Component({
+//  selector: "app",
+//  templateUrl: "template.html"
+//})
+//export class ProductComponent {
+//  model: Model = new Model();
+
+//  constructor(ref: ApplicationRef) {
+//    (<any>window).appRef = ref;
+//    (<any>window).model = this.model;
+//  }
+
+//  getProductByPosition(position: number): Product {
+//    return this.model.getProducts()[position];
+//  }
+
+//  getClassesByPosition(position: number): string {
+//    let product = this.getProductByPosition(position);
+//    return "p-2 " + (product.price < 50 ? "bg-info" : "bg-warning");
+//  }
+//}
 
 //import { Model } from "./repository.model";
 //import { Component } from "@angular/core";
@@ -59,4 +108,4 @@ export class ProductComponent {
 //  }
 
 //}
-
+//
