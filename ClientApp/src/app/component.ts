479ad6c9dcd81e4ba9bf9df47a68a57b85fb5f42
hanichu <hendrymino@gmail.com>
2019-07-08 14:59:30 +0200

    Updated template.html and component.ts

--- ClientApp/src/app/component.ts
+++ ClientApp/src/app/component.ts
@@ -1,5 +1,6 @@
+import { ApplicationRef, Component } from "@angular/core";
 import { Model } from "./repository.model";
-import { Component } from "@angular/core";
+import { Product } from "./product.model";
 
 @Component({
   selector: "app",
@@ -8,7 +9,54 @@ import { Component } from "@angular/core";
 export class ProductComponent {
   model: Model = new Model();
 
-  getClasses(): string {
-    return this.model.getProducts().length == 5 ? "bg-success" : "bg-warning";
+  constructor(ref: ApplicationRef) {
+    (<any>window).appRef = ref;
+    (<any>window).model = this.model;
+  }
+
+  getProductByPosition(position: number): Product {
+    return this.model.getProducts()[position];
+  }
+
+  getClassesByPosition(position: number): string {
+    let product = this.getProductByPosition(position);
+    return "p-2 " + (product.price < 50 ? "bg-info" : "bg-warning");
   }
 }
+
+//import { Model } from "./repository.model";
+//import { Component } from "@angular/core";
+
+//@Component({
+//  selector: "app",
+//  templateUrl: "template.html"
+//})
+//export class ProductComponent {
+//  model: Model = new Model();
+
+//  getClasses(): string {
+//    return this.model.getProducts().length == 5 ? "bg-success" : "bg-warning";
+//  }
+
+//  getClassMap(key: number): Object {
+//    let product = this.model.getProduct(key);
+//    return {
+//      "text-center bg-danger": product.name == "Kayak",
+//      "bg-info": product.price < 50
+//    }
+//  }
+
+//  fontSizeWithUnits: string = "30px";
+//  fontSizeWithoutUnits: string = "30";
+
+//  getStyles(key: number): Object {
+//    let product = this.model.getProduct(key);
+//    return {
+//      fontSize: "30px",
+//      "margin.px": 100,
+//      color: product.price > 50 ? "red" : "green"
+//    }
+//  }
+
+//}
+
