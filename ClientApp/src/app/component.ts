9c68a094893a543c593a1b9534184a958e317742
hanichu <hendrymino@gmail.com>
2019-07-09 12:01:07 +0200

    Updated till module 6

--- ClientApp/src/app/component.ts
+++ ClientApp/src/app/component.ts
@@ -2,6 +2,7 @@ import { ApplicationRef, Component } from "@angular/core";
 import { Model } from "./repository.model";
 import { Product } from "./product.model";
 import { ProceduralRenderer3 } from "@angular/core/src/render3/renderer";
+import { NgForm } from "@angular/forms"
 
 @Component({
   selector: "app",
@@ -27,11 +28,57 @@ export class ProductComponent {
     return this.model.getProducts();
   }
 
-  selectedProduct: string;
+  newProduct: Product = new Product();
 
-  getSelected(product: Product): boolean {
-    return product.name == this.selectedProduct;
+  get jsonProduct() {
+    return JSON.stringify(this.newProduct);
   }
+
+  addProduct(p: Product) {
+    console.log("new product: " + this.jsonProduct);
+  }
+
+  getValidationMessages(state: any, thingName?: string) {
+    let thing: string = state.path || thingName;
+    let messages: string[] = [];
+    if (state.errors) {
+      for (let errorName in state.errors) {
+        switch (errorName) {
+          case "required":
+            messages.push(`You must enter a ${thing}`);
+            break;
+          case "minlength":
+            messages.push(`A ${thing} must be at least ${state.errors['minlength'].requiredLength} characters`);
+            break;
+          case "pattern":
+            messages.push(`The ${thing} contains illegal characters`);
+            break;
+        }
+      }
+    }
+    return messages;
+  }
+
+  formSubmitted: boolean = false;
+
+  submitForm(form: NgForm) {
+    this.formSubmitted = true;
+
+    if (form.valid) {
+      this.addProduct(this.newProduct);
+
+      this.newProduct = new Product();
+      form.reset();
+
+      this.formSubmitted = false;
+    }
+  }
+
+  //selectedProduct: string;
+
+  //getSelected(product: Product): boolean {
+  //  return product.name == this.selectedProduct;
+  //}
 
   //  getProductCount(): number {
   //    console.log("getProductCount invoked");
