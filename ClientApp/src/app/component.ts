92b413e7f3bcb5de7e82568eef62574c802cc4de
hanichu <hendrymino@gmail.com>
2019-07-09 12:10:20 +0200

    Changed tamplate.html and component.ts from module 7

--- ClientApp/src/app/component.ts
+++ ClientApp/src/app/component.ts
@@ -1,8 +1,7 @@
 import { ApplicationRef, Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 import { Model } from "./repository.model";
 import { Product } from "./product.model";
-import { ProceduralRenderer3 } from "@angular/core/src/render3/renderer";
-import { NgForm } from "@angular/forms"
 
 @Component({
   selector: "app",
@@ -10,15 +9,6 @@ import { NgForm } from "@angular/forms"
 })
 export class ProductComponent {
   model: Model = new Model();
-
-  //constructor(ref: ApplicationRef) {
-  //  (<any>window).appRef = ref;
-  //  (<any>window).model = this.model;
-  //}
-
-  //getProductByPosition(position: number): Product {
-  //  return this.model.getProducts()[position];
-  //}
 
   getProduct(key: number): Product {
     return this.model.getProduct(key);
@@ -35,7 +25,7 @@ export class ProductComponent {
   }
 
   addProduct(p: Product) {
-    console.log("new product: " + this.jsonProduct);
+    console.log("New Product: " + this.jsonProduct);
   }
 
   getValidationMessages(state: any, thingName?: string) {
@@ -63,103 +53,20 @@ export class ProductComponent {
 
   submitForm(form: NgForm) {
     this.formSubmitted = true;
-
     if (form.valid) {
       this.addProduct(this.newProduct);
-
       this.newProduct = new Product();
       form.reset();
-
       this.formSubmitted = false;
     }
   }
 
-  //selectedProduct: string;
-
-  //getSelected(product: Product): boolean {
-  //  return product.name == this.selectedProduct;
-  //}
-
-  //  getProductCount(): number {
-  //    console.log("getProductCount invoked");
-  //    return this.getProducts().length;
-  //  }
-
-  //  targetName: string = "Kayak";
-
-  //  counter: number = 1;
-
-  //  get nextProduct(): Product {
-  //    return this.model.getProducts().shift();
-  //  }
-
-  //  getProductPrice(index: number): number {
-  //    return Math.floor(this.getProduct(index).price);
-  //  }
+  getFormValidationMessages(form: NgForm): string[] {
+    let messages: string[] = [];
+    Object.keys(form.controls).forEach(k => {
+      this.getValidationMessages(form.controls[k], k)
+        .forEach(m => messages.push(m));
+    });
+    return messages;
+  }
 }
-
-
-
-
-//import { ApplicationRef, Component } from "@angular/core";
-//import { Model } from "./repository.model";
-//import { Product } from "./product.model";
-
-//@Component({
-//  selector: "app",
-//  templateUrl: "template.html"
-//})
-//export class ProductComponent {
-//  model: Model = new Model();
-
-//  constructor(ref: ApplicationRef) {
-//    (<any>window).appRef = ref;
-//    (<any>window).model = this.model;
-//  }
-
-//  getProductByPosition(position: number): Product {
-//    return this.model.getProducts()[position];
-//  }
-
-//  getClassesByPosition(position: number): string {
-//    let product = this.getProductByPosition(position);
-//    return "p-2 " + (product.price < 50 ? "bg-info" : "bg-warning");
-//  }
-//}
-
-//import { Model } from "./repository.model";
-//import { Component } from "@angular/core";
-
-//@Component({
-//  selector: "app",
-//  templateUrl: "template.html"
-//})
-//export class ProductComponent {
-//  model: Model = new Model();
-
-//  getClasses(): string {
-//    return this.model.getProducts().length == 5 ? "bg-success" : "bg-warning";
-//  }
-
-//  getClassMap(key: number): Object {
-//    let product = this.model.getProduct(key);
-//    return {
-//      "text-center bg-danger": product.name == "Kayak",
-//      "bg-info": product.price < 50
-//    }
-//  }
-
-//  fontSizeWithUnits: string = "30px";
-//  fontSizeWithoutUnits: string = "30";
-
-//  getStyles(key: number): Object {
-//    let product = this.model.getProduct(key);
-//    return {
-//      fontSize: "30px",
-//      "margin.px": 100,
-//      color: product.price > 50 ? "red" : "green"
-//    }
-//  }
-
-//}
-//
